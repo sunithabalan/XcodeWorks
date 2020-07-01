5dc666a1619c92f1619970557725fca5995d610e
sunithabalasubramanian <sunithabalasubramanian14@gmail.com>
2020-07-01 10:45:47 +0530

    Add files via upload

---
+++ project6-Autolayout/project6-Autolayout/ViewController.swift
@@ -0,0 +1,75 @@
+//
+//  ViewController.swift
+//  project6-Autolayout
+//
+//  Created by Sunitha Balasubramanian on 23/06/20.
+//  Copyright © 2020 Sunitha Balasubramanian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+          let label1 = UILabel()
+        label1.translatesAutoresizingMaskIntoConstraints = false
+        label1.backgroundColor = UIColor.red
+        label1.text = "These"
+        label1.sizeToFit()
+
+            let label2 = UILabel()
+            label2.translatesAutoresizingMaskIntoConstraints = false
+            label2.backgroundColor = UIColor.cyan
+            label2.text = "ARE"
+            label2.sizeToFit()
+
+            let label3 = UILabel()
+            label3.translatesAutoresizingMaskIntoConstraints = false
+            label3.backgroundColor = UIColor.yellow
+            label3.text = "SOME"
+            label3.sizeToFit()
+
+            let label4 = UILabel()
+            label4.translatesAutoresizingMaskIntoConstraints = false
+            label4.backgroundColor = UIColor.green
+            label4.text = "AWESOME"
+            label4.sizeToFit()
+
+            let label5 = UILabel()
+            label5.translatesAutoresizingMaskIntoConstraints = false
+            label5.backgroundColor = UIColor.orange
+            label5.text = "LABELS"
+            label5.sizeToFit()
+
+            view.addSubview(label1)
+            view.addSubview(label2)
+            view.addSubview(label3)
+            view.addSubview(label4)
+            view.addSubview(label5)
+//        let viewsDictionary = ["label1": label1, "label2": label2, "label3": label3, "label4": label4, "label5": label5]
+//        for label in viewsDictionary.keys {
+//            view.addConstraints( NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
+//        }
+//        let metrics = ["height":88]
+//         view.addConstraints( NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(height@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]-(>=10)-|", options: [], metrics: metrics, views: viewsDictionary))
+        var previous:UILabel?
+        for label in [label1,label2,label3,label4,label5]{
+            label.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+            label.heightAnchor.constraint(equalToConstant: 88).isActive = true
+            if let previous = previous{
+                label.topAnchor.constraint(equalTo: previous.bottomAnchor, constant: 10).isActive = true
+            }
+            else{
+                label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+
+            }
+            previous = label
+        }
+        }
+        // Do any additional setup after loading the view.
+    }
+
+
+
+
